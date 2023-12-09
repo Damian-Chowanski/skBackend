898793f3a513816b0cf6c76eb6c957d7cb469302
Damian-Chowanski <dchowanski30@gmail.com>
2023-12-09 21:54:57 +0100

    Warehouse Controller has beed created

---
+++ src/main/java/com/example/skBackend/controller/WarehouseController.java
@@ -0,0 +1,28 @@
+package com.example.skBackend.controller;
+
+import com.example.skBackend.entity.Warehouse;
+import com.example.skBackend.repository.WarehouseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class WarehouseController {
+
+    private final WarehouseRepository warehouseRepository;
+
+    @PostMapping("/employees")
+    Warehouse newWarehouse(@RequestBody Warehouse newWarehouse){return warehouseRepository.save(newWarehouse);}
+
+    @GetMapping("/employees")
+    List<Warehouse> warehouseList(){return warehouseRepository.findAll();}
+
+    @DeleteMapping("/employees")
+    ResponseEntity deleteWarehouse(@RequestBody Long idWarehouse){
+        warehouseRepository.deleteById(idWarehouse);
+        return ResponseEntity.ok().build();
+    }
+}
