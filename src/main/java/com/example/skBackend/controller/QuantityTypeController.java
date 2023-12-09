645de679fb1ce00e5dc7b918cab66f49c6bb8ef2
Damian-Chowanski <dchowanski30@gmail.com>
2023-12-09 21:50:28 +0100

    QuantityType Controller has beed created

--- src/main/java/com/example/skBackend/controller/QuantityTypeController.java
+++ src/main/java/com/example/skBackend/controller/QuantityTypeController.java
@@ -1,4 +1,28 @@
 package com.example.skBackend.controller;
 
+import com.example.skBackend.entity.QuantityType;
+import com.example.skBackend.repository.QuantityTypeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class QuantityTypeController {
+
+    private final QuantityTypeRepository quantityTypeRepository;
+
+    @PostMapping("/employees")
+    QuantityType newQuantityType(@RequestBody QuantityType newQuantityType){return quantityTypeRepository.save(newQuantityType);}
+
+    @GetMapping("/employee")
+    List<QuantityType> quantityTypeList() {return quantityTypeRepository.findAll();}
+
+    @DeleteMapping("/employess")
+    ResponseEntity deleteTypeQuantity(@RequestBody Long idTypeQuantity){
+        quantityTypeRepository.deleteById(idTypeQuantity);
+        return ResponseEntity.ok().build();
+    }
 }
