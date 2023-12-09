f93ed2df7e023fd212a64aeda11a44e4095eb76b
Damian-Chowanski <dchowanski30@gmail.com>
2023-12-09 21:16:11 +0100

    Created repositories for entities

---
+++ src/main/java/com/example/skBackend/repository/WarehouseRepository.java
@@ -0,0 +1,8 @@
+package com.example.skBackend.repository;
+
+import com.example.skBackend.entity.Warehouse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
+
+}
