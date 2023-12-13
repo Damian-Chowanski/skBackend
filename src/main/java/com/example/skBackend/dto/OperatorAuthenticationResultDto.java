27f92d2daa60e1e02410427c35cb23c7b70da5aa
Damian-Chowanski <dchowanski30@gmail.com>
2023-12-13 20:54:16 +0100

    DTO class have been created

---
+++ src/main/java/com/example/skBackend/dto/OperatorAuthenticationResultDto.java
@@ -0,0 +1,13 @@
+package com.example.skBackend.dto;
+
+import lombok.Data;
+
+@Data
+public class OperatorAuthenticationResultDto {
+
+    private long idOperator;
+    private String firstName;
+    private String lastName;
+    private boolean authenticated;
+
+}
